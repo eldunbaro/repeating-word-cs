e6d8645e4bc6913c8e635e452a603ac80e5ebe06
Roger Dunbar <roger.dunbar@dyson.com>
2022-03-28 12:01:37 +0100

    Initial commit

---
+++ RepeatingWord/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RepeatingWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wordFinder = new WordFinder();
+        }
+    }
+}
